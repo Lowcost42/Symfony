94f2b29337c933722c9cfc1bf461b98a1d3bd8b3
Lowcost42 <160510760+Lowcost42@users.noreply.github.com>
2024-03-15 15:29:49 +0100

    route

--- src/Controller/DiyController.php
+++ src/Controller/DiyController.php
@@ -4,18 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DiyController extends AbstractController
 {
-    #[Route('/travaux/{slug}-{}id', name: 'diy.show')]
-    public function index($request): Response
+    #[Route('/travaux/{slug}-{id}', name: 'diy.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9\-]+'])]
+    public function index(Request $request): Response
     {
-        // return $this->render('diy/index.html.twig', [
-        //     'controller_name' => 'DiyController',
-        // ]);
-
-
-        dd($request);
+        dd($request->attributes->get('slug'), $request->attributes->get('id'));
     }
 }
